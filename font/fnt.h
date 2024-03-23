90f066f60ab14aa4bb4c01e73eb92b46fb48317e
michael <mikehautman@gmail.com>
2024-03-22 23:46:01 -0400

    miscounted :[

--- font/fnt.h
+++ font/fnt.h
@@ -2,7 +2,7 @@
 // NOTE: consider using an Enum?
 // index in list is the same as index in eeprom, multiplied by the width of the character bitmap
 uint8_t char_width = 5;
-char fnt_lut[52] = {
+char fnt_lut[51] = {
 '!', '%', '&', ''', '+', ',', '-', '.', '/', '0', 
 '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', 
 ';', '<', '=', '>', '?', 'A', 'B', 'C', 'D', 'E', 
@@ -12,6 +12,6 @@ char fnt_lut[52] = {
 
 uint8_t characterLookup(char val){
     uint8_t i = 0;
-    while(fnt_lut[i] != val){i++;}
+    while(fnt_lut[i++] != val )
     return i*char_width;
 }
