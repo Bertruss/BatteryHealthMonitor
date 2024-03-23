93896ca4920aa1d669ecd6294ba252800acf9682
michael <mikehautman@gmail.com>
2024-03-22 23:30:23 -0400

    finishing up functions to write whole screen

--- bhmDisplay/bhmDisplay.c
+++ bhmDisplay/bhmDisplay.c
@@ -6,51 +6,80 @@
 // width = 25;
 // height = 4;
 /*
-|	Z |~~~~~~~~~~~~~~~~~| 100%                            
-|	est. time - ____  	
+|	Z |~~~~~~~~~~~~~~~~~| 100%
+|	VOLTAGE :  	
+|	EST. TIME : ____  	
 | 			[warning]
 */
 
-void display_write_char(char val){
-	//retrieve address of character bitmap from eeprom
-	uint8_t addr = characterLookup(val);
-	uint8_t char_buffer[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
-	if(val != ' '){
-		eeprom_read(&char_buffer, addr, 5);
+void update_display(uint8_t percent, bool warn, uint16_t voltage){
+	draw_percent_bar(percent);
+	draw_voltage(voltage);
+	// draw time estimate?
+	if(warn){
+		display_warning();
+	}else{
+		clear_warning();
 	}
-	uint8_t buff;
-	/* this thing is d u m b.
+}
+
+void write_char(char val){
+	// retrieve address of character bitmap from eeprom
+	uint8_t addr;
+	if(val != ' '){
+		addr = characterLookup(val);
+	}else{
+		addr = 255;
+	}
+	render_symbol(addr);
+}
+
+void render_symbol(uint8_t address){// 
+	/* 
+	/ this thing is d u m b.
 	/ let me explain:
 	/ the SSD1306 with 128x32 pixel oled screen technically has screen buffer large enough for a screen twice that size;
-	/ the same chip drives a 128x64 oled. So, you might assume, that must mean that only half the addressable buffer is used. 
+	/ the same chip drives a 128x64 oled. So, you might assume (like me), that must mean that only half the addressable buffer is used. 
 	/ You would be wrong.
 	/ No, instead, for reasons that are murky to me, they decided that it made more sense to map full bytes to nibbles, 
 	/ only drawing every other bit to the screen in this form 01010101b. So you either have to waste space pre-encoding your data
 	/ to expand every byte to two bytes, or you have to waste cycles splitting every byte into two bytes. leading to the monstrosity below.
-	/ admittedly this is compounded by the font needing to be flipped from its orientation in memory, but still.
+	/ admittedly this is compounded by the font needing to be flipped from its orientation in memory, the screen is written LSB first.
 	*/
-	for(uint8_t i = 0; i < 5; i++){
-		cursor_pg = 0;
-		//pull first column
-		buff =
-		((char_buffer[i] & 0x80) >> 7) +
-		((char_buffer[i] & 0x40) >> 4) +
-		((char_buffer[i] & 0x20) >> 1) +
-		((char_buffer[i] & 0x10) << 2);
-		//first write slice
-		SD1306_draw(buff);
-		SSD1306_set_cursor(++cursor_pg, cursor_col);
-		buff =
-		((char_buffer[i] & 0x08) >> 3) +
-		((char_buffer[i] & 0x04)) +
-		((char_buffer[i] & 0x02) << 3) +
-		((char_buffer[i] & 0x01) << 6);
-		SD1306_draw(buff);
-		SSD1306_set_cursor(--cursor_pg, ++cursor_col);
+	uint8_t buff;
+	uint8_t char_buffer[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
+	if(address != 255){
+		eeprom_read(&char_buffer, addr, 5);
+		for(uint8_t i = 0; i < 5; i++){
+			cursor_pg = 0;
+			//pull first column
+			buff =
+			((char_buffer[i] & 0x80) >> 7) +
+			((char_buffer[i] & 0x40) >> 4) +
+			((char_buffer[i] & 0x20) >> 1) +
+			((char_buffer[i] & 0x10) << 2);
+			//first write slice
+			SD1306_draw(buff);
+			SSD1306_set_cursor(++cursor_pg, cursor_col);
+			buff =
+			((char_buffer[i] & 0x08) >> 3) +
+			((char_buffer[i] & 0x04)) +
+			((char_buffer[i] & 0x02) << 3) +
+			((char_buffer[i] & 0x01) << 6);
+			SD1306_draw(buff);
+			SSD1306_set_cursor(--cursor_pg, ++cursor_col);
+		}
+	else{
+		for(uint8_t i = 0; i < 5; i++){
+			SD1306_draw(0x00);
+			SSD1306_set_cursor(++cursor_pg, cursor_col);
+			SD1306_draw(0x00);
+			SSD1306_set_cursor(--cursor_pg, ++cursor_col);
+		}
 	}
 }
 
-void display_write_char_2x(char val){
+void render_symbol_x2(char val){
 	// draw characters double size
 	// retrieve address of character bitmap from eeprom
 	uint8_t addr = characterLookup(val);
@@ -101,6 +130,7 @@ void display_write_char_2x(char val){
 }
 
 void draw_graphic(uint8_t addr){
+	// draw graphic selected from library 
 	SD1306_draw(graphics[addr]);
 	SSD1306_set_cursor(++cursor_pg, cursor_col);
 	SD1306_draw(graphics[addr+1]);
@@ -108,7 +138,7 @@ void draw_graphic(uint8_t addr){
 }
 
 void draw_percent_bar(uint8_t percent){
-//draw percent bar 0-100
+	// draw percent bar 0-100
 	//set cursor
 	SSD1306_set_cursor(0, 0);
 
@@ -123,10 +153,10 @@ void draw_percent_bar(uint8_t percent){
 	draw_graphic(0);
 	draw_graphic(1);
 	int i = 0;
-	for(; i < percent - 5; i++)}{
+	for(; i < percent - 5; i++){
 		draw_graphic(4);
 	}
-	for(; i < 95; i++)}{
+	for(; i < 95; i++){
 		draw_graphic(2);
 	}
 	//draw the endcap
@@ -135,7 +165,7 @@ void draw_percent_bar(uint8_t percent){
 	
 	// line spacer 
 	SSD1306_set_cursor(cursor_pg, ++cursor_col);
-	// Convert percent to text :[ 
+	display_percent(percent);	
 }
 
 void clear_warning(){
@@ -146,14 +176,13 @@ void clear_warning(){
 void display_warning(){
 	SSD1306_set_cursor(6, 0);
 	for(int i = 0; i < 24 ; i++){
-		display_write_char(warning_txt[i]);		
+		write_char(warning_txt[i]);		
 	}
 }
 
 void display_percent(uint8_t val){
-	// Converts uint8_t to num string
-	// Returns number of characters in return string
-
+	// writes out battery percentage estimate
+	uint8_t out[3] = {0x00, 0x00, 0x00}; 
 	uint8_t len = 0;
 	uint8_t temp = 0;
 	// ascii numbers start with address 0x0A in the font
@@ -161,6 +190,7 @@ void display_percent(uint8_t val){
 		out[0] = 0x0A + 1; "1"
 		out[1] = 0x0A; "0"
 		out[2] = 0x0A; "0"
+		len = 3;
 	}
 	else{
 		if (val>10){
@@ -171,4 +201,31 @@ void display_percent(uint8_t val){
 		out[len++] = val/1 + 0x0A;
 	}
 	
+	for(int i = 0; i < len; i++){
+		render_symbol(out[i]);
+	}
+	write_char('%');
+}
+
+void display_voltage(uint16_t val){
+	// convert val to display voltage
+	// TODO: rework with some bitwise operations. this may take a lot of cycles
+	// determine expected voltage range (8 - 6.4 for 2 cell?)
+	
+	uint8_t millivolt_quanta = 0x00; //TODO: unknown scaling
+	uint8_t quanta_num;
+	quanta_num = val/millivolt_quanta;
+	uint8_t v = quanta_num/100;
+	quanta_num -= v * 100;  
+	uint8_t cv = quanta_num/10
+	uint8_t mv -= cv * 10;  
+	SSD1306_set_cursor(2, 0);
+
+	for(int i = 0; i < 13; i++){
+		write_char(voltage_txt[i]);
+	}
+	render_symbol(0x0A + v)
+	write_char('.');
+	render_symbol(0x0A + cv);
+	render_symbol(0x0A + mv);
 }
