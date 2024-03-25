3337cbe6f5d28f59e48c18ff2a8a206643cb1839
michael <mikehautman@gmail.com>
2024-03-24 20:59:35 -0400

    outlining ADC usage

---
+++ TinyADC/TinyADC.c
@@ -0,0 +1,26 @@
+
+void adc_init(){
+// select REFS[2:0] in ADMUX, probably just vcc
+// maybe do a voltage differential measurement?
+
+// select Analog input channel ADC0-3 from MUX[3:0] in the ADMUX for single ended. 
+// different rules for double ended 
+
+
+}
+
+void adc_enable(){ 
+
+}
+
+void adc_pause(){ 
+
+}
+
+uint16_t adc_read_async(){ 
+
+}
+
+uint16_t adc_read_sync(){ 
+
+}
---
+++ TinyADC/TinyADC.h
@@ -0,0 +1,9 @@
+// programming interface for the ADC
+
+void adc_init();
+void adc_enable();
+void adc_pause();
+uint16_t adc_read_async();
+uint16_t adc_read_sync();
+
+
