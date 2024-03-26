a6163f89ca950327186de3d6213f22feb7e36c0e
Bertruss <mikehautman@gmail.com>
2024-03-26 00:07:13 -0400

    added some compile guards

--- TinyEeprom/TinyEeprom.h
+++ TinyEeprom/TinyEeprom.h
@@ -1,4 +1,7 @@
+#ifndef TINY_EEPROM_H
+#define TINY_EEPROM_H
 #include <avr/io.h>
 
 uint8_t* eeprom_read(uint8_t start_addr, uint8_t length); 
 uint8_t eeprom_read_byte(uint8_t add);
+#endif
--- font/fnt.h
+++ font/fnt.h
@@ -1,4 +1,5 @@
-
+#ifndef FNT_H
+#define FNT_H
 // NOTE: consider using an Enum?
 // index in list is the same as index in eeprom, multiplied by the width of the character bitmap
 uint8_t char_width = 5;
@@ -15,3 +16,4 @@ uint8_t characterLookup(char val){
     while(fnt_lut[i++] != val )
     return i*char_width;
 }
+#endif
