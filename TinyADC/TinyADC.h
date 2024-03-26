e738fc7d9d6cc777fd36b81bfdeb12903e66cdcf
michael <mikehautman@gmail.com>
2024-03-25 22:15:24 -0400

    I made a new branch for this but that may have been unnecessary. this is fairly simple

--- TinyADC/TinyADC.h
+++ TinyADC/TinyADC.h
@@ -1,9 +1,10 @@
-// programming interface for the ADC
+#ifndef TINY_ADC_H
+#define TINY_ADC_H
+// interface for ease of use of the ADC
 
 void adc_init();
 void adc_enable();
 void adc_pause();
 uint16_t adc_read_async();
 uint16_t adc_read_sync();
-
-
+#endif
