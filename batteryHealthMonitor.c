fcb3adbf40e2d24268a59d7254b2584d20de1137
Bertruss <mikehautman@gmail.com>
2024-04-06 14:47:41 -0400

    removed some innaccurate comments from the main source file.

--- batteryHealthMonitor.c
+++ batteryHealthMonitor.c
@@ -1,24 +1,18 @@
 #include "./TinyTWI/fcpu.h"
 #include "./TinyTWI/TinyTWI.h"
-#include "./bhmDisplay/bhmDisplay.h"
+#include "./bhmUtils/bhmDisplay.h"
+#include "./bhmUtils/bhmUtils.h"
 #include "./SSD1306_driver/SSD1306_driver.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
-// the setup function runs once when you press reset or power the board
 int main(){
-    //initialize i2c
-    twi_init();
-    adc_init();
-	SSD1306_init();
-	SSD1306_clear();
+    twi_init(); // Initialize i2c
+    adc_init(); // Configure ADC
+    SSD1306_init(); // Setup the screen
+	SSD1306_clear(); // Clear the screen mem
 	
     while(1){
-        //ADC
-        // 10-bit
-        // 1024 steps
-        // resolution ~5mv (4.882813) at 5v vcc
-        //measure battery voltage
         uint32_t voltage = read_battery_voltage();
 
 		for(uint8_t i = 0; i < 101; i++){
@@ -27,14 +21,12 @@ int main(){
         //calculate battery percentage estimate
         //calculate time-to-empty
         //update screen
-        //** ads **
+        
         // measure cell voltage
         // transmit "bat low" if needed
         // low power shut-off?
         // low power beep?
-        // sleep for 500ms?update screen
-        
-        // testing the screen update
-        
+        // sleep for 500ms? update screen
     }
 }
+
