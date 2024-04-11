99bb99f21e12143bbd54cb270ce5b6e0a6da920d
Bertruss <mikehautman@gmail.com>
2024-04-11 01:36:09 -0400

    slight rework, update_display for voltage from 16 to 32 bit

--- batteryHealthMonitor.c
+++ batteryHealthMonitor.c
@@ -13,20 +13,22 @@ int main(){
 	SSD1306_clear(); // Clear the screen mem
 	
     while(1){
-        uint32_t voltage = read_battery_voltage();
-
-		for(uint8_t i = 0; i < 101; i++){
-			update_display(i, false, i);
-		}		
-        //calculate battery percentage estimate
+        uint32_t voltage = measure_battery_voltage();
+        uint8_t percent_v = calculate_battery_percentage(voltage);
+        bool batt_warn = percent_v > 15 ? false : true; // below 15% trigger battery warning
+		bool batt_warn_display = false;
+		update_display(percent_v, batt_warn, (uint16_t)(voltage >> 16));
+		//calculate battery percentage estimate
         //calculate time-to-empty
         //update screen
         
-        // measure cell voltage
-        // transmit "bat low" if needed
-        // low power shut-off?
-        // low power beep?
-        // sleep for 500ms? update screen
+		// ideas:
+        // measure cell voltage O
+        // transmit "bat low" if needed O
+        // low power shut-off? X
+        // low power beep? X
+        // sleep for 500ms? update screen O
+		//		- Set a timer based interrupt to kick off a measurement every 500ms
     }
 }
 
