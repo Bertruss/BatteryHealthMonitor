889ce5b62a7443cac191e031622cc032d4bedb59
Bertruss <mikehautman@gmail.com>
2024-04-11 01:33:39 -0400

    Fixing adc conversion, adding back reference voltage which is accurate to the function of the bhm circuitry

--- bhmUtils/bhmUtils.c
+++ bhmUtils/bhmUtils.c
@@ -9,15 +9,18 @@
 
 uint32_t measure_battery_voltage(){
     // measuring Vcc to apply Vref correction (should be ~5v but this step corrects for some innaccuracy)
-    uint32_t ref = 1.1*10^4/adc_measure_vcc()*1024/5; // measure Vcc error from 5v
+    uint32_t ref = 1.1*10000/adc_measure_ref()*1024;
     
-    // Apply error correction
+    // Apply error correction and reference offset
+
     // Note: quanta is just hard-coded 5/1024
-    uint32_t val = (adc_read_sync()*qaunta)/10^4*ref;
+    // ref SHOULD equal current Vcc, so the error correction modifier is the ref/5 
+    uint32_t val = (adc_read_sync()*qaunta)/10000/5*ref + ref;
 
     return val;
 }
 
 uint8_t battery_percentage(uint32_t voltage){
+    // TODO: battery discharge is only linearish down to 3.6v... 
     return (maxV/voltage);
 }
