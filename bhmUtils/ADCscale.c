820ddaf7b1bd9e23dfcf9e99ea543ec600cdfc50
Bertruss <mikehautman@gmail.com>
2024-04-11 01:29:24 -0400

    there's no power operator in c -_-

--- bhmUtils/ADCscale.c
+++ bhmUtils/ADCscale.c
@@ -1,4 +1,4 @@
 #include <stdint.h>
-uint32_t maxV = 8.4*10^8;
-uint32_t minV = 6.4*10^8;
-uint32_t qaunta = 5*10^8/1024;
+uint32_t maxV = 8.4*100000000; // 8.4*10^8
+uint32_t minV = 6.4*100000000; // 6.4*10^8
+uint32_t qaunta = 5*100000000/1024; // 5*10^8
