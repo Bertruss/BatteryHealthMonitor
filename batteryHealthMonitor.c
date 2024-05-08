fd36f0f1ad9300518abaf936fec77a4d7e08c620
Bertruss <mikehautman@gmail.com>
2024-05-07 21:36:42 -0400

    interrupt test

--- batteryHealthMonitor.c
+++ batteryHealthMonitor.c
@@ -8,6 +8,7 @@
 
 ISR(TIMER0_COMPA){ // toggle off the interrupt
     TIMSK &= !(1 << OCIE0A); // output compare timer0 registerA interrupt disable
+	TIFR // clear the flag
 }
 
 int main(){
@@ -16,14 +17,15 @@ int main(){
     adc_init(); // Configure ADC
     SSD1306_init(); // Setup the screen
 	SSD1306_clear(); // Clear the screen mem
+	configure_timer_interrupt(); // configure timer based interrupt to wake sleep
 	sei(); // global interrupt enable
 
     while(1){
         // Use the ADC noise canceler?
-        uint32_t voltage = measure_battery_voltage();
-        uint8_t percent_v = battery_percentage(voltage);
-        bool batt_warn = percent_v > 15 ? false : true; // below 15% trigger battery warning
-		bool batt_warn_display = false;
+        //uint32_t voltage = measure_battery_voltage();
+        //uint8_t percent_v = battery_percentage(voltage);
+        //bool batt_warn = percent_v > 15 ? false : true; // below 15% trigger battery warning
+		//bool batt_warn_display = false;
 		
 		//calculate battery percentage estimate
         //calculate time-to-empty
@@ -36,16 +38,14 @@ int main(){
         // low power beep? X
         // sleep for 500ms? update screen O
 		//		- Set a timer based interrupt to kick off a measurement every 500ms
-        update_display(percent_v, batt_warn, voltage);
+        //update_display(percent_v, batt_warn, voltage);
+		configure_timer_interrupt();
+		update_display(percent_v, batt_warn, voltage)
     }
 }
 
 void configure_timer_interrupt(){
-    MCUCR &= !(1 << SM1) &
-            !(1 << SM0); // set idle interrupt
-
     // PRR |= () //consider further power reduction
-
     TCCR1 |= (1 << CTC1) | // set timer clear on compare match
         (1 << CS13) |
         (1 << CS12) |
@@ -54,3 +54,4 @@ void configure_timer_interrupt(){
     OCR1A = 0xF4; //244; at 8mhz, divided by 16384, roughly a 1/2 second
     TIMSK |= (1 << OCIE0A); // output compare timer0 registerA interrupt enable
 }
+
