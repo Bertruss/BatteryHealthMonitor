7ba11a23c3b7de955fd732e9b516b89d38099831
Bertruss <mikehautman@gmail.com>
2024-05-08 10:31:43 -0400

    fixing syntax errors

--- bhmUtils/ADCscale.c
+++ bhmUtils/ADCscale.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-uint8_t cell_num = 2; // change to reflect specific battery parameters
+#define cell_num 2 // change to reflect specific battery parameters
 uint32_t maxV = cell_num*4.2*100000000; // C*4.2*10^8
 uint32_t minV = cell_num*3.2*100000000; // C*3.2*10^8
 uint32_t qaunta = 5*100000000/1024; // 5*10^8
--- bhmUtils/bhmUtils.c
+++ bhmUtils/bhmUtils.c
@@ -3,21 +3,23 @@
 #include "..\TinyADC\TinyADC.h"
 #include <stdint.h>
 
-// Note: to avoid floating point numbers, 32 bit unsigned int was used to preserve accuracy.
+// Note: to avoid floating point numbers, using fixed point math. 32-bit unsigned int was used to preserve accuracy (and conserve memory)
 // TODO: consider uint64_t, operations had to have their accuracy pre-halved to avoid overflows. (using 10^4 instead of 10^8)
 // though it should be noted, this does seem to give at least 2 decimal places free of quantization error.
 
 uint32_t measure_battery_voltage(){
     // measuring Vcc to apply Vref correction (should be ~5v but this step corrects for some innaccuracy)
-    if(adc_activepin != ADC3_PB3){
+    if(adc_active_pin != ADC3_PB3){
         adc_pin_select(ADC3_PB3);
     }
-    uint32_t ref = 1.1*10^4/adc_measure_vcc()*1024; // measure Vcc error from 5v
+	
+	// NOTE: not sure there is a better way to do this to limit quantization error
+    uint32_t ref = 1.1*10000/adc_measure_ref()*1024; // measure Vcc error from 5v. 
     
     // Apply error correction
     // Note: quanta is just hard-coded 5/1024
     // add vcc offset to get 
-    uint32_t val = (adc_read(WAKE)*qaunta)/10^4*ref/5 + ref;
+    uint32_t val = (adc_read(WAKE)*qaunta)/10000*ref/5 + ref;
     return val;
 }
 
