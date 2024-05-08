b5c6e2fa9dbec350676653a32d6b2ae9b4879b1a
michael <mikehautman@gmail.com>
2024-05-07 20:48:07 -0400

    flashing out ADC changes to utilization stages

--- bhmUtils/bhmUtils.c
+++ bhmUtils/bhmUtils.c
@@ -8,23 +8,22 @@
 // though it should be noted, this does seem to give at least 2 decimal places free of quantization error.
 
 uint32_t measure_battery_voltage(){
-	adc_enable();
-	
     // measuring Vcc to apply Vref correction (should be ~5v but this step corrects for some innaccuracy)
-    uint32_t ref = 1.1*10000/adc_measure_ref()*1024;
+    if(adc_activepin != ADC3_PB3){
+        adc_pin_select(ADC3_PB3);
+    }
+    uint32_t ref = 1.1*10^4/adc_measure_vcc()*1024; // measure Vcc error from 5v
     
-    // Apply error correction and reference offset
-
+    // Apply error correction
     // Note: quanta is just hard-coded 5/1024
-    // ref SHOULD equal current Vcc, so the error correction modifier is the ref/5 
-    uint32_t val = (adc_read(SLEEP)*qaunta)/10000/5*ref + ref;
-	
-	// power-saving, disable ADC
-	adc_pause();
+    // add vcc offset to get 
+    uint32_t val = (adc_read(WAKE)*qaunta)/10^4*ref/5 + ref;
     return val;
 }
 
-uint8_t battery_percentage(uint32_t voltage){
-    // TODO: battery discharge is only linearish down to 3.6v... 
-    return (maxV/voltage);
+uint8_t battery_charge(uint32_t voltage){
+    // "open circuit voltage" battery charge
+    //4.2 - 3.6 90%
+    //3.6 - 3.2 10%
+    return 0; //yeah we're not doing this yet
 }
