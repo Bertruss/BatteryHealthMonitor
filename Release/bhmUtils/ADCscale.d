b580c1ba28bf023a6cf8c9e45d420dfdc289214d
Bertruss <mikehautman@gmail.com>
2024-06-05 01:04:45 -0400

    untracking some unintentionally tracked dev files

--- Release/bhmUtils/ADCscale.d
+++
@@ -1,7 +0,0 @@
-bhmUtils/ADCscale.d bhmUtils/ADCscale.o: ../bhmUtils/ADCscale.c \
- c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h
-
-c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h:
--- Release/font/fnt.d
+++
@@ -1,9 +0,0 @@
-font/fnt.d font/fnt.o: ../font/fnt.c ../font/fnt.h \
- c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h
-
-../font/fnt.h:
-
-c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h:
