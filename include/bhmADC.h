881c6b2a5f10fff43ba9d6e569c4dfced7835905
Bertruss <mikehautman@gmail.com>
2025-03-06 23:39:39 -0500

    added several useful defines. 1e8 fixed point. look up table to map voltages to charge estimate.

--- include/bhmADC.h
+++ include/bhmADC.h
@@ -1,8 +1,10 @@
 #ifndef ADC_SCALE_H
 #define ADC_SCALE_H
 #include <stdint.h>
-extern uint32_t maxV;
-extern uint32_t minV;
+#define cell_num 2 // change to reflect specific battery parameters
+#define fixed_point_1e8 (uint32_t)100000000
 extern uint32_t qaunta;
-extern uint32_t Ah;
+extern uint64_t Ah;
+extern uint16_t cellV_LUT[18];
+
 #endif
