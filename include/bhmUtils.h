5ed53f812189ee6d3726ad914069c61ad4aa6682
Bertruss <mikehautman@gmail.com>
2025-03-06 23:43:36 -0500

    added current draw measurement, added multi-modal charge estimation

--- include/bhmUtils.h
+++ include/bhmUtils.h
@@ -2,6 +2,9 @@
 #define BHM_UTILS_H
 #include <stdint.h>
 void bhm_init();
+uint32_t measure_vcc_voltage();
 uint32_t measure_battery_voltage();
-uint8_t battery_charge(uint32_t voltage);
+uint32_t measure_current_draw();
+uint8_t basic_charge_estimate(uint32_t voltage);
+uint8_t adv_charge_estimate(uint32_t voltage, uint32_t current, uint64_t *estimated_energy);
 #endif
