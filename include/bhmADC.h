c157641eee50edbf0d15bbc7a3a2d1ba84e09e86
Bertruss <mikehautman@gmail.com>
2025-03-30 15:02:12 -0400

    changed constant vars to Const, which I didn't think was necessary

--- include/bhmADC.h
+++ include/bhmADC.h
@@ -4,8 +4,8 @@
 #define cell_num 2 // change to reflect specific battery parameters
 // fixed point set at 27th bit. 6-bits of integer, 26-bits for fractional values. Minimum real resolution of ~1.5e-8. Integer range from 63-0.
 #define fixed_point (uint32_t) 0x04000000
-extern uint32_t qaunta;
-extern uint64_t Ah;
-extern uint16_t cellV_LUT[21];
-extern uint32_t amp_per_v;
+extern const uint32_t qaunta;
+extern const uint64_t Ah;
+extern const uint16_t cellV_LUT[21];
+extern const uint32_t amp_per_v;
 #endif
