dc824bce179dec03b6d936bb7caa192d581a3934
Bertruss <mikehautman@gmail.com>
2025-03-30 15:02:55 -0400

    added single character delete function, which might be superfluous but whatever

--- include/SSD1306_driver.h
+++ include/SSD1306_driver.h
@@ -11,5 +11,6 @@ void SSD1306_set_cursor(uint8_t page, uint8_t col);
 void SSD1306_reset_cursor();
 void SSD1306_clear();
 void SSD1306_clear_segment(uint8_t pg, uint8_t start_col, uint8_t end_col);
+void SSD1306_delete();
 void SSD1306_set_brightness(uint8_t val);
 #endif
