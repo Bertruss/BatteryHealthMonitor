4243b226205ee3828bbef60f26432ce07bfeb854
Bertruss <mikehautman@gmail.com>
2025-03-30 15:15:51 -0400

    WIP testing commit

--- src/bhmADC.c
+++ src/bhmADC.c
@@ -1,11 +1,10 @@
 #include <stdint.h>
 #include "../include/bhmADC.h"
-uint32_t qaunta = 5*fixed_point/1024; // Volts per step,  with 5v Vcc
-uint64_t Ah = 2*fixed_point*3600; // should be set to match the storage capacity of the battery, in Ah, converted to As
+const uint32_t qaunta = 5*fixed_point/1024; // Volts per step,  with 5v Vcc
+const uint64_t Ah = 2*fixed_point*3600; // should be set to match the storage capacity of the battery, in Ah, converted to As
 // N*10^-2. 100%, down in %5increments.
-
 //https://blog.ampow.com/lipo-voltage-chart/
-uint16_t cellV_LUT[21] = {
+const uint16_t cellV_LUT[21] = {
 	420*cell_num, 415*cell_num, 411*cell_num, 408*cell_num, 
 	402*cell_num, 389*cell_num, 395*cell_num, 391*cell_num, 
 	387*cell_num, 385*cell_num, 384*cell_num, 382*cell_num, 
@@ -16,4 +15,4 @@ uint16_t cellV_LUT[21] = {
 	// sense resistor gain is 11, sense resistor is .01R
 	// V = 11*.01*I
 	// I = V/(11*.01)
-uint32_t amp_per_v = (fixed_point)/11*100;
+const uint32_t amp_per_v = (fixed_point)/11*100;
