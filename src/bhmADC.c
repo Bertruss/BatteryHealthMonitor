266c8b06e516159a285f698c3f75f536607e5dd7
Bertruss <mikehautman@gmail.com>
2025-04-26 11:50:20 -0400

    removed lookuptable. switching to nonlinear approximation based on best fit to the lookuptables data.

--- src/bhmADC.c
+++ src/bhmADC.c
@@ -1,20 +1,11 @@
 #include <stdint.h>
 #include "../include/bhmADC.h"
-const uint32_t qaunta = 5*fixed_point/1024; // Volts per step,  with 5v Vcc
-const uint64_t Ah = 2*fixed_point*3600; // should be set to match the storage capacity of the battery, in Ah, converted to As
-// N*10^-2. 100%, down in %5increments.
-//https://blog.ampow.com/lipo-voltage-chart/
-const uint16_t cellV_LUT[21] = {
-	420*cell_num, 415*cell_num, 411*cell_num, 408*cell_num, 
-	402*cell_num, 398*cell_num, 395*cell_num, 391*cell_num, 
-	387*cell_num, 385*cell_num, 384*cell_num, 382*cell_num, 
-	380*cell_num, 379*cell_num, 377*cell_num, 375*cell_num, 
-	373*cell_num, 371*cell_num, 369*cell_num, 361*cell_num,
-	327*cell_num};
-	
+#include "../include/TinyMath.h"
+uint32_t qaunta = fix(5)/1024; // Volts per step,  with 5v Vcc
+uint64_t Ah = fix(2)*3600; // should be set to match the storage capacity of the battery, in Ah, converted to As
 	// sense resistor gain is 11, sense resistor is .01R
 	// V = 11*.01*I
 	// I = V/(11*.01)
-const uint32_t amp_per_v = (fixed_point)/11*100;
+uint32_t amp_per_v = fix(1)/11*100;
 
 
